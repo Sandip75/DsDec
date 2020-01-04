cc3f9a27b9ec67b4e9a876419afb22ef2bc638e1
Sandip75 <sk1957339@gmail.com>
2020-01-04 16:55:33 +0530

    class2

---
+++ class2/p5.java
@@ -0,0 +1,35 @@
+package class2;
+
+import java.util.*;
+
+public class p5 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int star = 1;
+		int dot = n - 1;
+		int i = 0;
+		while (i <= (2 * (n - 1))) {
+			int j = 0;
+			while (j < dot) {
+				System.out.print(" ");
+				j++;
+			}
+			while (j < (dot + star)) {
+				System.out.print("*");
+				j++;
+			}
+			if (i >= (n-1)) {
+				star -= 2;
+				dot += 1;
+			} else {
+				star += 2;
+				dot -= 1;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
